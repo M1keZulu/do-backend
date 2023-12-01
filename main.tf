8f1910091af994b22d4c982f4c2f2f07258ff08c
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-01 18:27:41 +0500

    Update Terraform

--- main.tf
+++ main.tf
@@ -18,14 +18,9 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
-}
-
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm.DO_Project.location
+  resource_group_name = azurerm.DO_Project.name
 }
