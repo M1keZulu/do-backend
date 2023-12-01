459a698cd39146da1ca46289fc6857ce59ee2253
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-02 00:24:25 +0500

    Update api

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, this is a simple Flask API!'
+    return 'Hello, this is a simple Flask API! Updated'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
