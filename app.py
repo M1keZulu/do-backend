99a66f0933b737b4da2fc37de0abebddbdb56c3d
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-01 23:40:57 +0500

    Add backend api

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py (Flask API)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, this is a simple Flask API!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
