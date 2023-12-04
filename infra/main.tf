aa462adc4d17a870bfd78850419f8877d5292e3a
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-04 17:21:03 +0500

    Infra udpate

--- infra/main.tf
+++ infra/main.tf
@@ -44,27 +44,3 @@ resource "azurerm_container_registry" "tempcoreg" {
   sku                      = "Basic"
   admin_enabled            = true
 }
-
-
-resource "azurerm_kubernetes_cluster" "tempkube" {
-  name                = "tempkube"
-  location            = azurerm_resource_group.temprsg.location
-  resource_group_name = azurerm_resource_group.temprsg.name
-  dns_prefix          = "tempkube" // Replace with your preferred DNS prefix
-  kubernetes_version  = "1.21.4"
-
-  default_node_pool {
-    name       = "default"
-    node_count = 3
-    vm_size    = "Standard_DS2_v2" // Choose the appropriate VM size
-  }
-
-  service_principal {
-    client_id     = var.client_id
-    client_secret = var.client_secret
-  }
-
-  tags = {
-    environment = "production"
-  }
-}
