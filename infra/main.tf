eb2d7797a671da5e30fe1bcb39b60f83586c419f
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-04 15:14:48 +0500

    Update infra

--- infra/main.tf
+++ infra/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_storage_container" "tempcon" {
 }
 
 resource "azurerm_container_registry" "tempcoreg" {
-  name                     = "tempreg"
+  name                     = "tempcoreg"
   resource_group_name      = azurerm_resource_group.temprsg.name
   location                 = azurerm_resource_group.temprsg.location
   sku                      = "Basic"
