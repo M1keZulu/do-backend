6777cdac3792b9304f081f715c78fd2961f68233
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-04 14:54:34 +0500

    Infrastructure testing

---
+++ infra/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.27.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = var.bkstrgrg
+    storage_account_name = var.bkstrg
+    container_name       = var.bkcontainer
+    key                  = var.bkstrgkey
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+}
+
+resource "azurerm_resource_group" "temprsg" {
+  name     = "temprsg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "tempsag" {
+  name                     = "tempsag"
+  resource_group_name      = azurerm_resource_group.temprsg.name
+  location                 = azurerm_resource_group.temprsg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "docon" {
+  name                  = "docon"
+  storage_account_name  = azurerm_storage_account.tempsag.name
+  container_access_type = "private"
+}
+
+resource "azurerm_container_registry" "doreg" {
+  name                     = "doreg"
+  resource_group_name      = azurerm_resource_group.temprsg.name
+  location                 = azurerm_resource_group.temprsg.location
+  sku                      = "Basic"
+  admin_enabled            = true
+}
