19f40befbf066fef0393b8c483357823351e1423
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-04 16:40:23 +0500

    Update infra

--- infra/main.tf
+++ infra/main.tf
@@ -22,3 +22,26 @@ resource "azurerm_resource_group" "example-group" {
   name     = "example-group"
   location = "East US"
 }
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "tempkube"
+  location            = azurerm_resource_group.temprsg.location
+  resource_group_name = azurerm_resource_group.temprsg.name
+  dns_prefix          = "tempkube" // Replace with your preferred DNS prefix
+  kubernetes_version  = "1.21.4"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 3
+    vm_size    = "Standard_DS2_v2" // Choose the appropriate VM size
+  }
+
+  service_principal {
+    client_id     = var.client_id
+    client_secret = var.client_secret
+  }
+
+  tags = {
+    environment = "production"
+  }
+}
