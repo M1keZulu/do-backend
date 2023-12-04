59c22f702dbb8c8a90437f3cd6c174c04a125c52
Muhammad Zain Ul Abiddin <muhammadzain757@gmail.com>
2023-12-04 16:42:59 +0500

    Update infra

--- infra/main.tf
+++ infra/main.tf
@@ -18,10 +18,33 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example-group" {
-  name     = "example-group"
+resource "azurerm_resource_group" "temprsg" {
+  name     = "temprsg"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "tempsag" {
+  name                     = "tempsag"
+  resource_group_name      = azurerm_resource_group.temprsg.name
+  location                 = azurerm_resource_group.temprsg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "tempcon" {
+  name                  = "tempcon"
+  storage_account_name  = azurerm_storage_account.tempsag.name
+  container_access_type = "private"
+}
+
+resource "azurerm_container_registry" "tempcoreg" {
+  name                     = "tempcoreg"
+  resource_group_name      = azurerm_resource_group.temprsg.name
+  location                 = azurerm_resource_group.temprsg.location
+  sku                      = "Basic"
+  admin_enabled            = true
+}
+
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "tempkube"
